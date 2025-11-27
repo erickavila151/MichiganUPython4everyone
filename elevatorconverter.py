31f6e5585f58b01ef84617a89fccacbee9f4517b
Erick Avila <erickavila414@gmail.com>
2025-11-27 12:49:45 -0600

    primer commit

---
+++ elevatorconverter.py
@@ -0,0 +1,4 @@
+#Convert elevator floors
+inp =input('Please introduce the European floor number: ')
+usf = int (inp) + 1
+print('That floor in the US is:', usf)
---
+++ hellosarah.py
@@ -0,0 +1,4 @@
+#Genera un codigo que pida al usuario su nombre y luego imprima un saludo personalizado.
+name = input("Please introduce your name: ")
+welcome = "Hello, " + name + "! Welcome!"
+print(welcome)
