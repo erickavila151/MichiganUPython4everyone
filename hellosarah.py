8f12efbb98b0f40f9424a8aa58e8c7a97fb1bc8a
Erick Avila <erickavila414@gmail.com>
2025-11-27 12:58:44 -0600

    test of commits

--- hellosarah.py
+++ hellosarah.py
@@ -1,4 +1,4 @@
 #Genera un codigo que pida al usuario su nombre y luego imprima un saludo personalizado.
 name = input("Please introduce your name: ")
-welcome = "Hello, " + name + "! Welcome!"
+welcome = "Hello, " + name + "! Welcome to our show!"
 print(welcome)
