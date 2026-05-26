ca6640e951dd905d752f5b35495eac0f5ded7c9a
Erick Avila <erickavila414@gmail.com>
2026-05-25 20:05:41 -0600

    exercise course 3 completed

--- course3/exercises/exercise2/readandprocess.py
+++ course3/exercises/exercise2/readandprocess.py
@@ -1,8 +1,23 @@
 # Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
+count = 0
+total = 0.0
+
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:"):
         continue
-    print(line)
-print("Done")
+
+    value = float(line.split(":")[1])
+    total = total + value
+    count = count + 1
+
+average = total / count
+
+print("Average spam confidence:", average)
+
+
+    
+    
+    
+
